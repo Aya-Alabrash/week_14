6476175b709594003e483499b88c8d077272cecd
Aya-Alabrash <aya026688@gmail.com>
2023-06-15 21:12:22 +0200

    Add send email & main example to call it.

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ form get_covid_cases import *
 from get_weather import *
 
 def main():
-  country = "canada"
-  print(get_covid_cases(country))
-  print(get_weather(country))
+    country = "canada"
+    print(get_covid_cases(country))
+    print(get_weather(country))
+
+    message = f"\nLocation: {location}\n\n{weather_info}\n\n{covid_info}"
+    # Your Email details
+    sender_email = "Your_Email@gmail.com"
+    sender_password = "Your_password/App_password"
+    receiver_email = "Receiver@gmail.com"
+    subject = f"Update for {location}"
+
+    # Send the email
+    send_email(sender_email, sender_password, receiver_email, subject, message)
 
 if __name__ == '__main__':
-  main()
+    main()
---
+++ send_email.py
@@ -0,0 +1,31 @@
+def send_email(email, password, recipient, subject, message):
+    
+    gmail_user = email
+    gmail_app_password = password
+    
+    sent_from = gmail_user
+    sent_to = [recipient, recipient]
+    
+    sent_subject = subject
+    
+    sent_body = message.encode('ascii', 'ignore').decode('ascii')
+    
+    email_text = """\
+                From: %s
+                To: %s
+                Subject: %s
+
+                %s
+                """ % (sent_from, ", ".join(sent_to), sent_subject, sent_body)
+    
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_app_password)
+        server.sendmail(sent_from, sent_to, email_text)
+        server.close()
+
+        print('Email sent!')
+        
+    except Exception as exception:
+        print("Error: %s!\n\n" % exception)
